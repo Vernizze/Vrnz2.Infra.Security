6d9ced750b2e134ecd794bae8f812d31fd865218
Vernizze <cfvernizze@gmail.com>
2021-01-04 11:03:08 -0300

    Implementacao do type 'Pwd' e de criacao de hash via PBKDF2

---
+++ Vrnz2.Infra.Security.Test/Types/PwdTest.cs
@@ -0,0 +1,42 @@
+﻿using Vrnz2.Infra.Security.Types;
+using Xunit;
+
+namespace Vrnz2.Infra.Security.Test.Types
+{
+    public class PwdTest
+    {
+        [Theory]
+        [InlineData("#123Abcd")]
+        public void ValidPwd_StringValue_Valid(string value)
+        {
+            Pwd pwd = value;
+
+            Assert.True(pwd.IsValid);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("@1A")]
+        [InlineData("2123Abcd")]
+        [InlineData("#123A cd")]
+        [InlineData("#adadAbcd")]
+        [InlineData("#123abcd")]
+        public void ValidPwd_StringValue_NotValid(string value)
+        {
+            Pwd pwd = value;
+
+            Assert.False(pwd.IsValid);
+        }
+
+        [Theory]
+        [InlineData("#123Abcd", "#123Abcd")]
+        public void ValidPwd_Compare_Success(string value01, string value02)
+        {
+            Pwd pwd01 = value01;
+            Pwd pwd02 = value02;
+
+            Assert.NotEqual(pwd01.Value, pwd02.Value);
+        }
+    }
+}
---
+++ Vrnz2.Infra.Security/Types/Pwd.cs
@@ -0,0 +1,61 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using Vrnz2.Infra.CrossCutting.Extensions;
+using Vrnz2.Infra.Security.Libraries;
+
+namespace Vrnz2.Infra.Security.Types
+{
+    public struct Pwd
+    {
+        #region Constants
+
+        private const string SPECIAL_CHARS = @"\|!#$%&/()=?»«@£§€{}.-;'<>_,";
+
+        #endregion 
+
+        #region Atributes
+
+        public readonly bool IsValid { get; }
+
+        public readonly string Value { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public Pwd(string value)
+            : this()
+        {
+            this.IsValid = Valid(value);
+
+            if (this.IsValid)
+                this.Value = PBKDF2.Compute(value);
+        }
+
+        #endregion
+
+        #region Operators
+
+        public static implicit operator Pwd(string value)
+            => new Pwd(value);
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Static methods
+
+        public static bool Valid(string value)
+            =>
+                !string.IsNullOrEmpty(value) &&                                                 //Not Null or Empty
+                (value.ToCharArray().Intersect(SPECIAL_CHARS.ToCharArray())).HaveAny() &&       //Must have Special Character
+                new Regex(@"[0-9]+").IsMatch(value) &&                                          //Must have Number
+                new Regex(@"[A-Z]+").IsMatch(value) &&                                          //Must have Upper Case Letter
+                new Regex(@".{8,}").IsMatch(value) &&                                           //Must have eight characters 
+                !(new Regex(@" ").IsMatch(value));                                              //Don't have white spaces
+
+        #endregion        
+    }
+}
